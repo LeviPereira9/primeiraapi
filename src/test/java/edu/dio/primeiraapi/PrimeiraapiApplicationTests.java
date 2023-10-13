fd505083de27704847f5ab85ea73547c3b95a008
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2023-10-13 16:41:21 -0300

    first commit

---
+++ src/test/java/edu/dio/primeiraapi/PrimeiraapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dio.primeiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
